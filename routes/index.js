269d311ef29fb8f85f546d9103ff101e25a3ddd5
Nicholas Gallardo <nic.gallardo@gmail.com>
2015-12-28 13:37:46 -0700

    connections list shows

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,25 @@ router.get('/', function(req, res, next) {
   })
 });
 
+router.get('/users/:id', function(req, res, next) {
+  knex('users').where({id: req.params.id}).first().then(function (user){
+
+    knex('connections')
+      .innerJoin('users', 'other_id', 'users.id')
+      .where({user_id: user.id})
+      .then(function (connections) {
+
+        knex('connections')
+          .innerJoin('users', 'user_id', 'users.id')
+          .where({other_id: user.id})
+          .then(function (connections2) {
+            res.render('user', { user: user, connections: connections.concat(connections2)});
+          })
+
+    })
+  })
+});
+
+
+
 module.exports = router;
